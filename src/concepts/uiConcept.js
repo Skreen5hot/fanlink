59102add378382dfdc3235ee5b612d2d18ff4112
Aaron Damiano <aaron.damiano@gmail.com>
2025-12-01 06:17:06 -0500

    Refactor projectConcept to uiConcept for UI management

---
+++ src/concepts/uiConcept.js
@@ -0,0 +1,6 @@
+/**
+ * @module uiConcept
+ * @description Manages all direct DOM manipulations and UI state.
+ * It listens to DOM events and notifies the system. It also exposes actions
+ * for other concepts to trigger UI updates via the synchronization layer.
+ */
